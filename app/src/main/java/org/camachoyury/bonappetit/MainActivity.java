b8889f9d3f5bc7f25deb2225953ff9896257d3f3
Yury Camacho <camachoyury@gmail.com>
2017-03-16 16:27:53 -0300

    Added facebook login activity, and add constraint layout support

--- app/src/main/java/org/camachoyury/bonappetit/MainActivity.java
+++ app/src/main/java/org/camachoyury/bonappetit/MainActivity.java
@@ -1,7 +1,12 @@
 package org.camachoyury.bonappetit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.facebook.AccessToken;
+
+import org.camachoyury.bonappetit.mvp.activities.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(AccessToken.getCurrentAccessToken() == null){
+
+            goLoginScreen();
+        }
+    }
+
+    public void goLoginScreen(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/org/camachoyury/bonappetit/mvp/activities/LoginActivity.java
+++ app/src/main/java/org/camachoyury/bonappetit/mvp/activities/LoginActivity.java
@@ -1,10 +1,67 @@
 package org.camachoyury.bonappetit.mvp.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.camachoyury.bonappetit.MainActivity;
+import org.camachoyury.bonappetit.R;
 
 /**
  * Created by yury on 3/16/17.
  */
 
 public class LoginActivity  extends AppCompatActivity {
+
+    private LoginButton loginButton;
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.fb_button);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                goMainScreen();
+            }
+
+            @Override
+            public void onCancel() {
+
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void goMainScreen(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
 }
